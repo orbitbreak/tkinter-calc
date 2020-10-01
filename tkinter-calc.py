e2cf5ba46d35db8e2130dbb746c12c6e95ec0569
sharklab7 <55528399+sharklab7@users.noreply.github.com>
2020-10-01 09:54:07 +0530

    Update tkinter-calc.py
    1. Fixing the Window size 
    2. Enlarging Font size of Buttons
    3. Padding added between buttons
    4. Grey background added
    5. Looks more good

--- tkinter-calc.py
+++ tkinter-calc.py
@@ -1,66 +1,74 @@
-import Tkinter as tk
+from tkinter import *
 
-calc = tk.Tk()
+calc = Tk()
 calc.title("CrappyCalc")
+calc.config(bg="grey")
+
+# default size of window
+calc.geometry("300x500")
+
+# avoid stretching of Window
+calc.maxsize(300,500)
+calc.minsize(300,500)
 
 buttons = [
-'7',  '8',  '9',  '*',  'C',
-'4',  '5',  '6',  '/',  'Neg',
-'1',  '2',  '3',  '-',  '$',
-'0',  '.',  '=',  '+',  '@' ]
+    '7', '8', '9', '*', 'C',
+    '4', '5', '6', '/', 'Neg',
+    '1', '2', '3', '-', '$',
+    '0', '.', '=', '+', '@']
 
 # set up GUI
 row = 1
 col = 0
 for i in buttons:
     button_style = 'raised'
-    action = lambda x = i: click_event(x)
-    tk.Button(calc, text = i, width = 7, height = 7, relief = button_style, command = action) \
-		.grid(row = row, column = col, sticky = 'nesw', )
+    action = lambda x=i: click_event(x)
+    Button(calc, text=i, width=1,height=5, relief=button_style, command=action,font="arial 13 bold") \
+        .grid(row=row, column=col, sticky='nesw',padx =1,pady=1)
     col += 1
     if col > 4:
         col = 0
         row += 1
 
-display = tk.Entry(calc, width = 40, bg = "white")
-display.grid(row = 0, column = 0, columnspan = 5)
+display = Entry(calc, width=50, bg="white")
+display.grid(row=0, column=0, columnspan=5,pady=5)
+
 
 def click_event(key):
-
-	# = -> calculate results
+    # = -> calculate results
     if key == '=':
         # safeguard against integer division
         if '/' in display.get() and '.' not in display.get():
-            display.insert(tk.END, ".0")
-			
+            display.insert(END, ".0")
+
         # attempt to evaluate results
         try:
             result = eval(display.get())
-            display.insert(tk.END, " = " + str(result))
+            display.insert(END, " = " + str(result))
         except:
-            display.insert(tk.END, "   Error, use only valid chars")
-			
-	# C -> clear display		
+            display.insert(END, "   Error, use only valid chars")
+
+    # C -> clear display		
     elif key == 'C':
-        display.delete(0, tk.END)
-		
-		
-	# $ -> clear display		
+        display.delete(0,END)
+
+
+    # $ -> clear display		
     elif key == '$':
-        display.delete(0, tk.END)
-        display.insert(tk.END, "$$$$C.$R.$E.$A.$M.$$$$")
-		
+        display.delete(0, END)
+        display.insert(END, "$$$$C.$R.$E.$A.$M.$$$$")
 
-	# @ -> clear display		
+
+    # @ -> clear display		
     elif key == '@':
-        display.delete(0, tk.END)
-        display.insert(tk.END, "wwwwwwwwwwwwwwwwebsite")		
+        display.delete(0, END)
+        display.insert(END, "wwwwwwwwwwwwwwwwebsite")
 
-		
-	# neg -> negate term
+
+    # neg -> negate term
     elif key == 'neg':
         if '=' in display.get():
-            display.delete(0, tk.END)
+            display.delete(0, END)
         try:
             if display.get()[0] == '-':
                 display.delete(0)
@@ -69,11 +77,12 @@ def click_event(key):
         except IndexError:
             pass
 
-	# clear display and start new input		
+    # clear display and start new input		
     else:
         if '=' in display.get():
-            display.delete(0, tk.END)
-        display.insert(tk.END, key)
+            display.delete(0, END)
+        display.insert(END, key)
+
 
 # RUNTIME
 calc.mainloop()
